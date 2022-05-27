ca3f4b1ecfacc9089046af7938b15d318e18720a
huynhle29951 <huynhle29951@gmail.com>
2022-05-27 20:28:53 +0700

    Video-3-Create-React-App

---
+++ test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import moduleName from 'module';
+
+import React from 'react'
+
+export default function test() {
+  return (
+    <div>test</div>
+  )
+}
+
+function App() {
+  return <div className="test">hello world</div>;
+}
+
+export default App;
